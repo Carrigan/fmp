10fc7aede3a545ab1006d10ab5971faac5a57db9
Brian Carrigan <brian@bcarrigan.com>
2021-07-22 11:28:32 -0400

    Add ability to extract date from YAML and filename

---
+++ src/date.rs
@@ -0,0 +1,96 @@
+use super::yaml_rust::{Yaml};
+use super::Regex;
+
+#[derive(Debug)]
+pub struct Date {
+    pub year: u8,
+    pub month: u8,
+    pub day: u8
+}
+
+impl Date {
+    pub fn from_filename(filename: &str) -> Option<Date> {
+        let filename_regex = Regex::new(r"^(\d{6})").ok()?;
+        let captured_integer = filename_regex
+            .captures(filename)?
+            .get(0)?
+            .as_str()
+            .parse::<i64>()
+            .ok()?;
+
+        Some(Date::from_i64(captured_integer))
+    }
+
+    pub fn from_yaml(yaml: &Yaml) -> Option<Date> {
+        if let Some(integer_date) = yaml["date"].as_i64() {
+            return Some(Date::from_i64(integer_date));
+        }
+
+        if let Some(string_date) = yaml["date"].as_str() {
+            let integer_date = string_date.parse::<i64>().ok()?;
+            return Some(Date::from_i64(integer_date));
+        }
+
+        None
+    }
+
+    fn from_i64(integer_date: i64) -> Date {
+        Date {
+            year: (integer_date / 10000) as u8,
+            month: ((integer_date / 100) % 100) as u8,
+            day: (integer_date % 100) as u8
+        }
+    }
+}
+
+#[test]
+fn test_date_from_yaml_number() {
+    let content = String::from("date: 210820");
+    let yml = yaml_rust::YamlLoader::load_from_str(&content).unwrap().pop().unwrap();
+    let date = Date::from_yaml(&yml).unwrap();
+    assert_eq!(date.year, 21);
+    assert_eq!(date.month, 8);
+    assert_eq!(date.day, 20);
+}
+
+#[test]
+fn test_date_from_yaml_string() {
+    let content = String::from("date: \"210820\"");
+    let yml = yaml_rust::YamlLoader::load_from_str(&content).unwrap().pop().unwrap();
+    let date = Date::from_yaml(&yml).unwrap();
+    assert_eq!(date.year, 21);
+    assert_eq!(date.month, 8);
+    assert_eq!(date.day, 20);
+}
+
+#[test]
+fn test_date_from_yaml_absent() {
+    let content = String::from("something: else");
+    let yml = yaml_rust::YamlLoader::load_from_str(&content).unwrap().pop().unwrap();
+    let date = Date::from_yaml(&yml);
+    assert!(date.is_none());
+}
+
+#[test]
+fn test_date_from_yaml_bad() {
+    let content = String::from("date: bad");
+    let yml = yaml_rust::YamlLoader::load_from_str(&content).unwrap().pop().unwrap();
+    let date = Date::from_yaml(&yml);
+    assert!(date.is_none());
+}
+
+#[test]
+fn test_date_from_filename_good() {
+    let filename = "210820.md";
+    let date = Date::from_filename(&filename).unwrap();
+    assert_eq!(date.year, 21);
+    assert_eq!(date.month, 8);
+    assert_eq!(date.day, 20);
+}
+
+#[test]
+fn test_date_from_filename_bad() {
+    let filename = "120-bad-file.md";
+    let date = Date::from_filename(&filename);
+    assert!(date.is_none());
+}
---
+++ src/frontmatter.rs
@@ -0,0 +1,52 @@
+use std::{fs, path::PathBuf};
+use super::Date;
+use super::Regex;
+
+#[derive(Debug)]
+pub struct Frontmatter {
+    path: String,
+    yaml: yaml_rust::yaml::Yaml,
+    date: Date
+}
+
+impl Frontmatter {
+    pub fn load(path: &PathBuf) -> Option<Frontmatter> {
+        let content = match path.extension().map(|p| p.to_str().unwrap()) {
+            Some("yml") => fs::read_to_string(path).ok(),
+            Some("md") => Frontmatter::extract_from_md(&fs::read_to_string(path).unwrap()),
+            _ => None
+        }?;
+
+        let mut yamls = yaml_rust::YamlLoader::load_from_str(&content).ok()?;
+        let yaml = yamls.pop()?;
+
+        let filename_date = path
+            .file_name()
+            .map(|filename| filename.to_str())
+            .flatten()
+            .map(|filename| Date::from_filename(filename))
+            .flatten();
+
+        let date = match (Date::from_yaml(&yaml), filename_date) {
+            (Some(date), _) => Some(date),
+            (None, Some(date)) => Some(date),
+            _ => None
+        }?;
+
+        let path_string = String::from(path.to_str()?);
+
+        Some(Frontmatter { path: path_string, yaml, date })
+    }
+
+    fn extract_from_md(text: &str) -> Option<String> {
+        let frontmatter_regex = Regex::new(r"^---\r?\n*([\s\S]*)\r?\n---\r?\n").unwrap();
+        frontmatter_regex.captures(text)?.get(1).map(|cap| String::from(cap.as_str()))
+    }
+}
+
+#[test]
+fn test_extract_frontmatter() {
+    let expected_extracted = "date: 210720\nrunning:\n  distance: 4.6\n  time: \"32:45\"";
+    let test_markdown = fs::read_to_string("fixtures/test_frontmatter.md").unwrap();
+    assert_eq!(Frontmatter::extract_from_md(&test_markdown).unwrap(), expected_extracted);
+}
